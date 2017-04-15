76bcb2829bedb258784e14ef80cae22f86e318da
Matthew Hassell <hassell.matthew@gmail.com>
2017-04-15 15:17:10 -0400

    playing with facial stuff

--- myFaceRecognizer.py
+++ myFaceRecognizer.py
@@ -1,18 +1,20 @@
-# http://hanzratech.in/2015/02/03/face-recognition-using-opencv.html
+import os
 
-import cv2, os
-import numpy as np 
+import cv2
+import numpy as np
 from PIL import Image
+
+
+'''Where the pictures are stored'''
+train_directory = "2017-04-15"
+test_directory = "test"
 
 cascadePath = "haarcascade_frontalface_default.xml" 
 faceCascade = cv2.CascadeClassifier(cascadePath);
-
 recognizer = cv2.createLBPHFaceRecognizer()
-#recognizer = cv2.createEigenFaceRecognizer()   # these need some additional structure
-#recognizer = cv2.createFisherFaceRecognizer()
 
-def get_images_and_labels(path):
-	image_paths = [os.path.join(path, f) for f in os.listdir(path) if not f.endswith('.sad')]
+def get_images(directory):
+	image_paths = [os.path.join(directory, f) for f in os.listdir(directory)]
 	images = []
 	labels = []
 	for image_path in image_paths:
@@ -21,7 +23,7 @@ def get_images_and_labels(path):
 		except:
 			continue
 		image = np.array(image_pil, 'uint8')
-		nbr = int(os.path.split(image_path)[1].split(".")[0].replace("subject", ""))
+		nbr = int(os.path.split(image_path)[1].split(".")[0].replace("image", ""))
 		faces = faceCascade.detectMultiScale(image)
 		for (x, y, w, h) in faces:
 			images.append(image[y: y + h, x: x + w])
@@ -31,13 +33,11 @@ def get_images_and_labels(path):
 
 	return images, labels
 
-path = 'yalefaces'
-
-images, labels = get_images_and_labels(path)
+images, labels = get_images(train_directory)
 cv2.destroyAllWindows()
 recognizer.train(images, np.array(labels))
 
-image_paths = [os.path.join(path, f) for f in os.listdir(path) if f.endswith('.sad')]
+image_paths = [os.path.join(test_directory, f) for f in os.listdir(test_directory)]
 
 for image_path in image_paths:
 	predict_image_pil = Image.open(image_path).convert('L')
@@ -45,19 +45,13 @@ for image_path in image_paths:
 	faces = faceCascade.detectMultiScale(predict_image)
 	for (x, y, w, h) in faces:
 		nbr_predicted, conf = recognizer.predict(predict_image[y: y + h, x: x + w])
-		nbr_actual = int(os.path.split(image_path)[1].split(".")[0].replace("subject", ""))
-		if nbr_actual == nbr_predicted:
-			print "{} is Correctly Recognized with confidence {}".format(nbr_actual, conf)
-
-		else:
-			print "{} is Incorrectly Recognized as {}".format(nbr_actual, nbr_predicted)
+		print conf
+		#nbr_actual = int(os.path.split(image_path)[1].split(".")[0].replace("image", ""))
+		#if nbr_actual == nbr_predicted:
+		#	print "{} is Correctly Recognized with confidence {}".format(nbr_actual, conf)
+		#
+		#else:
+		#	print "{} is Incorrectly Recognized as {}".format(nbr_actual, nbr_predicted)
 
 		cv2.imshow("Recognizing Face", predict_image[y: y + h, x: x + w])
 		cv2.waitKey(1000)
-
-
-
-
-
-
-
--- takePictures.py
+++ takePictures.py
@@ -1,11 +1,13 @@
 # from here: https://codeplasma.com/2012/12/03/getting-webcam-images-with-python-and-opencv-2-for-real-this-time/
 
-import cv2
 import os
+
 from  datetime import date, datetime
 import time
+import cv2
 
-file = date.today().isoformat()
+#file = date.today().isoformat()
+file = 'test'
 try:
 	os.mkdir(file)
 except:
@@ -19,7 +21,10 @@ camera = cv2.VideoCapture(camera_port)
 
 def get_image():
 	retval,im = camera.read()
-	return im
+	if retval:
+		return im
+	else:
+		print 'error capturing image'
 
 # chuck the first few images while the camera adjusts
 for i in range(ramp_frames):
