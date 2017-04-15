9306baabdf55486ad3d0cdedd8775e771caf5b3b
Matthew Hassell <hassell.matthew@gmail.com>
2017-04-15 08:24:30 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Playing with opencv and python to try to make an automatic login based on facial recognition
---
+++ takePictures.py
@@ -0,0 +1,37 @@
+# from here: https://codeplasma.com/2012/12/03/getting-webcam-images-with-python-and-opencv-2-for-real-this-time/
+
+import cv2
+import os
+from  datetime import date, datetime
+import time
+
+file = date.today().isoformat()
+try:
+	os.mkdir(file)
+except:
+	pass
+
+camera_port = 0
+ramp_frames = 5
+num_pics = 10
+
+camera = cv2.VideoCapture(camera_port)
+
+def get_image():
+	retval,im = camera.read()
+	return im
+
+# chuck the first few images while the camera adjusts
+for i in range(ramp_frames):
+	temp=get_image()
+
+print "now taking images"
+
+for i in range(num_pics):
+	camera_capture = get_image()
+	filename = file + "/image" + str(i) + ".png"
+	print filename
+	cv2.imwrite(filename, camera_capture) 
+	cv2.waitKey(500)
+
+del(camera)
