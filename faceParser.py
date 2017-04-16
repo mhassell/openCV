8766ad74ebb973b6600e12194c815704767f03ab
Matthew Hassell <hassell.matthew@gmail.com>
2017-04-16 12:49:28 -0400

    still false positives with the eye cascade, hmmm

--- faceParser.py
+++ faceParser.py
@@ -6,25 +6,38 @@ import cv2
 import numpy as np
 from PIL import Image
 
+resize = False
+
+if resize:
+	dsize = (500,500)
 
 '''Where the pictures are stored'''
-directory = 'test'
+directory = 'train'
 
-cascadePath = "haarcascade_frontalface_default.xml" 
-faceCascade = cv2.CascadeClassifier(cascadePath);
+faceCascadePath = "haarcascade_frontalface_default.xml" 
+eyeCascadePath = 'haarcascade_eye.xml'
+faceCascade = cv2.CascadeClassifier(faceCascadePath);
+eyeCascade = cv2.CascadeClassifier(eyeCascadePath)
 recognizer = cv2.createLBPHFaceRecognizer()
 
 image_paths = [os.path.join(directory, f) for f in os.listdir(directory)]
 
 for image_path in image_paths:
-	try:
-		image_pil = Image.open(image_path).convert('L')
-	except:
+	if image_path == 'train/.DS_Store':
 		continue
+	image_pil = Image.open(image_path).convert('L')
 	image = np.array(image_pil, 'uint8')
-	faces = faceCascade.detectMultiScale(image)
+	if resize:
+		print(image.shape)
+		image = cv2.resize(image, dsize, interpolation = cv2.INTER_CUBIC)
+		cv2.imshow("resized image", image)
+	faces = faceCascade.detectMultiScale(image, minNeighbors=3, scaleFactor = 1.2)
+	eyes = eyeCascade.detectMultiScale(image, minNeighbors=5, scaleFactor = 1.2)
 	for (x, y, w, h) in faces:
-		cv2.imshow("Verifying face", image[y: y + h, x: x + w])
+		for (ex,ey,ew,eh) in eyes:
+			cv2.rectangle(image,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+		cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2)
+		cv2.imshow("Verifying face",  image) #image[y: y + h, x: x + w])
 		k = cv2.waitKey(0)
 		if k == 32: # space
 			continue
